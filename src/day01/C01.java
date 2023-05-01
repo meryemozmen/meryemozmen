04f298eecfd5e6905043129db83ac564f4d6c60f
meryemozmen <meryem201979@gmail.com>
2023-05-01 17:40:10 +0300

    first commit

---
+++ src/day01/C01.java
@@ -0,0 +1,6 @@
+package day01;
+
+public class C01 {
+   // \Users\HP\Desktop\Github Dersi
+}
+
---
+++ src/day01/C02.java
@@ -0,0 +1,36 @@
+package day01;
+
+public class C02 {
+
+    /*
+    git.init komutu, bir workingDirectory'u git ile irtibatlandırır bu koomut her proje için sadece bir kere yazılır
+      birden fazla yazıldığında ise  reinitialized  exid der bu komut ile birlikte git içinde bulundura klasörü kontrol
+      etmeye başlar
+      Bu klasörde her yedek almakj istediğimizde commit yapmalıyız
+      Aşağıdaki 3 komut bizim dosyalarımızı her yedeklediğmizde kullanmamız gereken komutlardır.
+
+      1- git add . :Son kaydedilen versiyondan sonra projede yapılan değişiklikleri  belirler
+
+      2-git commit -m "commit ismi" :
+        projenin son durumunu bir versiyon olarak kaydeder  burada "" içinde yazılan commit isminin anlamlı olmasında
+        fayda vardır. İlerleyen aşamalartda geri dönüş yapmak istersek hangi commit'e dönmek isteyeceğimizi açıklamadan
+        bulabiliriz.
+
+        git ile ilişiklendirdiğimiz dosyadaki class isimleri git durumuna göre renklenir
+         kırmızı :  bu class her hangi bir versişyona eklenmedi
+         mavi : bu dosya bir versiyona eklendi ama sonradan değişiklik yapılmış.
+         siyah : bu class bir versiyonda yedeklendi sonrasında hiç bir değişiklik yok
+
+      3- git push : Local Repoda commit vettiğimiz dosyalarımızı uzak masaüstü(GitHub)ne yollamak için kullanılır
+      Local Bilgisayarda oluşturduğuımuz bir repoyu GitHuba yollamak arada da yedeklemek istiyorsak öncelikle GitHub
+      hesabı oluşturmamız ve sonrasında projemizi göndereceğimiz GitHub repo sunu hazırlama
+      GitHub ta yeni br repository oluşturduğumuzda bir kereye mahsus ilk kullanımda lokal repository ile  global repository
+       repository ile arasında bağlantı kurmamız gerekir.
+
+     */
+
+
+    }
+
+
+
